f4b7882bee9a6692b7d4cf71e6025e66c9987442
ayan-gsynergy <ayan.agrawal@gsynergy.com>
2023-08-12 12:59:51 +0530

    first commit

---
+++ NumpyTest.py
@@ -0,0 +1,24 @@
+import numpy as np
+a=np.array([1,2,3,4,5])
+print(f"a = {a}")
+
+b=np.array([2,-2,3,0,5])
+
+c=a+b
+print(f"c = {c}")
+
+print(f"a-b = {a-b}")
+
+print(a[2:4])
+
+d=np.arange(10)
+
+try:
+    print(f"a+d = {a+d}")
+except:
+    print("the error msg")
+
+print(f"a-d = {a-d}")
+
+print("Hello there")
+
